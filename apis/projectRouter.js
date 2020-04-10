2911fb7c51f92b801d857f5ebe8cadb7d2b2df55
Gideon <gideon@Mac-1s-MacBook-Pro.local>
2020-04-10 14:19:39 -0400

    getting Project actions successfully

--- apis/projectRouter.js
+++ apis/projectRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
-const Project = require("../data/helpers/projectModel.js")
+const Project = require("../data/helpers/projectModel.js");
+const Action = require("../data/helpers/actionModel.js");
 
 const router = express.Router()
+
 
 router.get("/", (req, res) => {
     Project.get()
@@ -10,6 +12,61 @@ router.get("/", (req, res) => {
                res.status(200).json(project);
            })
 })
+
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    Project.get(id)
+           .then(project => {
+               res.status(200).json(project)
+           })
+})
+
+
+router.get("/:id/actions/:project_id", (req, res) => {
+    // const { description, notes } = req.body;
+    const { project_id} = req.params;
+    Project.getProjectActions(project_id)
+          .then(action => {
+              if(action) {
+                res.status(200).json(action)
+              } else {
+                res.status(404).json({error: "Not found"});
+              }
+          })
+          .catch(err => {
+              console.log(err)
+              res.status(500).json({error: "could be not retrieved from the database"})
+          })
+
+})
+
+// router.get("/:id/actions", (req, res) => {
+//     Action.get()
+//           .then(action => {
+//               res.status(200).json(action)
+//           })
+
+// })
+// router.get("/:id/actions/:action_id", (req, res) => {
+//     // const { description, notes } = req.body;
+//     const { action_id} = req.params;
+//     Action.get(action_id)
+//           .then(action => {
+//               if(action) {
+//                 res.status(200).json(action)
+//               } else {
+//                 res.status(404).json({error: "Not found"});
+//               }
+//           })
+//           .catch(err => {
+//               console.log(err)
+//               res.status(500).json({error: "could be not retrieved from the database"})
+//           })
+
+// })
+
+
 
 router.post("/", (req, res) => {
     const { name, description } = req.body;
@@ -33,6 +90,7 @@ router.put("/:id", (req, res) => {
                res.status(500).json({error: "Error updating Project data"});
            })
 })
+
 
 router.delete("/:id", (req, res) => {
     const { id } = req.params;
--- apis/server.js
+++ apis/server.js
@@ -4,8 +4,10 @@ const server = express();
 server.use(express.json())
 
 const projectRouter = require("./projectRouter.js")
+const actionRouter = require("./actionRouter.js")
 
 server.use("/api/users", projectRouter)
+
 
 server.get("/", (req, res) => {
     res.send("<h2>Hello from the localhost</h2>");
